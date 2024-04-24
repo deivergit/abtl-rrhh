9c0f9c34a31af6b236b72d8be3aa2fa7db692e99
Deiver Martinez <crrpersonal90@gmail.com>
2024-04-24 07:52:48 -0400

    Se agrego una validacion al momento de agregar un trabajador

--- controllers/agregar-trabajador-controller.php
+++ controllers/agregar-trabajador-controller.php
@@ -1,6 +1,7 @@
 <?php
 
 if (isset($_POST["registrar_trabajador"])) {
+   
    require_once("../config/database.php");
 
    # TABLE TRABAJADORES
@@ -33,25 +34,37 @@ if (isset($_POST["registrar_trabajador"])) {
    # TABLE SUELDOS TRABAJADORES
    $sueldo = $_POST["sueldo"];
 
-   $insert_table_trabajadores = "INSERT INTO trabajadores(fecha_ingreso, fecha_egreso, estatus, categoria) VALUES('$fecha_ingreso','$fecha_egreso','$estatus','$categoria')";
+   $comprobar_cedula_sql = "SELECT * FROM documentos_identidad_trabajadores WHERE numero_documento = '$numero_documento'";
+   $comprobar_cedula = mysqli_query($conn, $comprobar_cedula_sql);
 
-   if ($conn->query($insert_table_trabajadores)) {
+   if ($comprobar_cedula->num_rows > 0) {
 
-      $trabajador_fk = $conn->insert_id;
+      mysqli_close($conn);
+      echo "<script>
+    alert('LA CEDULA YA SE ENCUENTRA REGISTRADA');
+    window.location.href = '../trabajadores.php';
+    </script>";
+   } else {
+      $insert_table_trabajadores = "INSERT INTO trabajadores(fecha_ingreso, fecha_egreso, estatus, categoria) VALUES('$fecha_ingreso','$fecha_egreso','$estatus','$categoria')";
 
-      $insert_table_cargos_ejercidos = "INSERT INTO cargos_ejercidos(trabajador_fk, direccion_adscrita, cargo, fecha_inicio, fecha_fin) VALUES('$trabajador_fk', '$direccion_adscrita', '$cargo', '$fecha_inicio', '$fecha_fin')";
-      $conn->query($insert_table_cargos_ejercidos);
+      if ($conn->query($insert_table_trabajadores)) {
 
-      $insert_table_documentos_de_identidad = "INSERT INTO documentos_identidad_trabajadores(trabajador_fk, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, tipo_documento, numero_documento, pais_nacimiento, estado_civil, fecha_nacimiento, sexo) VALUES('$trabajador_fk','$primer_nombre','$segundo_nombre','$primer_apellido','$segundo_apellido', '$tipo_documento', '$numero_documento', '$pais_nacimiento', '$estado_civil', '$fecha_nacimiento', '$sexo')";
-      $conn->query($insert_table_documentos_de_identidad);
+         $trabajador_fk = $conn->insert_id;
 
-      $insert_table_escala_remuneracion = "INSERT INTO escala_remuneracion_trabajadores(trabajador_fk, escala_remuneracion) VALUES('$trabajador_fk', '$escala_remuneracion')";
-      $conn->query($insert_table_escala_remuneracion);
+         $insert_table_cargos_ejercidos = "INSERT INTO cargos_ejercidos(trabajador_fk, direccion_adscrita, cargo, fecha_inicio, fecha_fin) VALUES('$trabajador_fk', '$direccion_adscrita', '$cargo', '$fecha_inicio', '$fecha_fin')";
+         $conn->query($insert_table_cargos_ejercidos);
 
-      $insert_table_sueldos_trabajadores = "INSERT INTO sueldos_trabajadores(trabajador_fk, sueldo_base) VALUES('$trabajador_fk', '$sueldo')";
-      $conn->query($insert_table_sueldos_trabajadores);
+         $insert_table_documentos_de_identidad = "INSERT INTO documentos_identidad_trabajadores(trabajador_fk, primer_nombre, segundo_nombre, primer_apellido, segundo_apellido, tipo_documento, numero_documento, pais_nacimiento, estado_civil, fecha_nacimiento, sexo) VALUES('$trabajador_fk','$primer_nombre','$segundo_nombre','$primer_apellido','$segundo_apellido', '$tipo_documento', '$numero_documento', '$pais_nacimiento', '$estado_civil', '$fecha_nacimiento', '$sexo')";
+         $conn->query($insert_table_documentos_de_identidad);
+
+         $insert_table_escala_remuneracion = "INSERT INTO escala_remuneracion_trabajadores(trabajador_fk, escala_remuneracion) VALUES('$trabajador_fk', '$escala_remuneracion')";
+         $conn->query($insert_table_escala_remuneracion);
+
+         $insert_table_sueldos_trabajadores = "INSERT INTO sueldos_trabajadores(trabajador_fk, sueldo_base) VALUES('$trabajador_fk', '$sueldo')";
+         $conn->query($insert_table_sueldos_trabajadores);
+
+         mysqli_close($conn);
+         header("Location: ../trabajadores.php");
+      }
    }
-
-   mysqli_close($conn);
-   header("Location: ../trabajadores.php");
 }
